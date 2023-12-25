4189449e813016c706b72765e85ba11d77ee72bf
Spirin Sergey <73538964+SpirinSergey@users.noreply.github.com>
2023-12-25 13:34:41 +0300

    Update new_dir.py

--- Scripts/new_dir.py
+++ Scripts/new_dir.py
@@ -1,6 +1,7 @@
+import os
+
 
 def new_dir():
-    import os
     name_dir = input('Название новой папки:\n')
     for i in range(int(input("Сколько создать папок?\n"))):
         new_d = os.path.join(os.getcwd(), name_dir + "_" + str(i + 1))
